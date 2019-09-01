97d06fb5177f69a928993fa0e72c3d5cba2573f0
Chris <christianbuchert@gmail.com>
2019-09-01 15:00:08 -0600

    Added support for

--- src/test/util/parseHexRgb.test.js
+++ src/test/util/parseHexRgb.test.js
@@ -13,10 +13,12 @@ describe("parseHexRgba", () => {
         expect(decRgb1.r).toEqual(255);
         expect(decRgb1.g).toEqual(255);
         expect(decRgb1.b).toEqual(255);
+        expect(decRgb1.a).toEqual(255);
 
         expect(decRgb2.r).toEqual(0);
         expect(decRgb2.g).toEqual(0);
         expect(decRgb2.b).toEqual(0);
+        expect(decRgb1.a).toEqual(255);
     });
 
     it("parses three character hex strings.", () => {
@@ -26,14 +28,21 @@ describe("parseHexRgba", () => {
         expect(decRgb1.r).toEqual(255);
         expect(decRgb1.g).toEqual(255);
         expect(decRgb1.b).toEqual(255);
+        expect(decRgb1.a).toEqual(255);
 
         expect(decRgb2.r).toEqual(0);
         expect(decRgb2.g).toEqual(0);
         expect(decRgb2.b).toEqual(0);
+        expect(decRgb2.a).toEqual(255);
     });
 
     it("strips off leading # symbols.", () => {
         expect(parseHexRgba("#ff0000").r).toEqual(255);
         expect(parseHexRgba("#f00").r).toEqual(255);
+    });
+
+    it("adds an alpha channel when passed one.", () => {
+        expect(parseHexRgba("ffffff00").a).toEqual(0);
+        expect(parseHexRgba("fff0").a).toEqual(0);
     });
 });
--- src/util/parseHexRgba.js
+++ src/util/parseHexRgba.js
@@ -13,19 +13,21 @@ export default function parseHexRgba(color) {
 }
 
 function hexToRgbObject(hex) {
-    const hexWordLength = hex.length / 3;
+    const hexWordCount = hex.length % 3 === 0 ? 3 : 4;
+    const hexWordLength = hex.length / hexWordCount;
     const hexValues = stringToNLengthElementArray(hex, hexWordLength);
-    const  hexObject = {
+    const hexObject = {
         r: hexWordToDec(hexValues[0]),
         g: hexWordToDec(hexValues[1]),
         b: hexWordToDec(hexValues[2]),
+        a: hexWordCount === 3? 255 : hexWordToDec(hexValues[3]),
     };
 
     return hexObject;
 }
 
 function hexWordToDec(word) {
-    const repeatCount = word.length === 1? 2: 1;
+    const repeatCount = word.length === 1 ? 2 : 1;
 
     return (hexToDec(word.repeat(repeatCount)));
 }
