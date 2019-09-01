d094e3e7c5822953e34f6b0c570cc24b9755cd7e
Chris <christianbuchert@gmail.com>
2019-09-01 15:10:51 -0600

    An RGBA color can be initialized from a string or an RGBA object.

--- src/models/RGBAColor.js
+++ src/models/RGBAColor.js
@@ -1,8 +1,23 @@
+import parseHexRgba from "../util/parseHexRgba";
+
 export default class RGBAColor {
     constructor(color) {
         this.r = 0;
         this.g = 0;
         this.b = 0;
         this.a = 0;
+
+        if (typeof color === "string") {
+            this.setRgba(parseHexRgba(color));
+        } else if (color.hasOwnProperty("r") && color.hasOwnProperty("g") && color.hasOwnProperty("b")) {
+            this.setRgba(color);
+        }
+    }
+
+    setRgba({r, g, b, a}) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
     }
 }
