4ebde1195aaaf4c8e17644651f08a598dc3294f4
Chris <christianbuchert@gmail.com>
2019-09-01 19:44:39 -0600

    RGBAColor can be constructed with a hex string or an RGB object.

--- src/test/models/RGBAColor.test.js
+++ src/test/models/RGBAColor.test.js
@@ -5,4 +5,22 @@ describe("RGBAColor", () => {
     it("throws an error if not passed a value.", () => {
         expect(() => new RGBAColor()).toThrow(errors.noValueSupplied);
     });
+
+    it("builds a RGBA color when passed a hex string.", () => {
+        const color = new RGBAColor("AABBCCDD");
+
+        expect(color.r).toEqual(170);
+        expect(color.g).toEqual(187);
+        expect(color.b).toEqual(204);
+        expect(color.a).toEqual(221);
+    });
+
+    it("builds a RGBA color when passed a color object.", () => {
+        const sourceObject = {r: 1, g: 10, b: 100};
+        const color = new RGBAColor(sourceObject);
+
+        expect(color.r).toEqual(sourceObject.r);
+        expect(color.g).toEqual(sourceObject.g);
+        expect(color.b).toEqual(sourceObject.b);
+    });
 });
